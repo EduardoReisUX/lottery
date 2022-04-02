c05a09aae1a8d36af9de227795efc19f799fb6b2
EduardoReisUX <ducabruno09@hotmail.com>
2022-04-02 09:22:21 -0400

    test: add config to mock imported styles and imgs

--- jest.config.cjs
+++ jest.config.cjs
@@ -6,6 +6,10 @@ module.exports = {
 	setupFilesAfterEnv: ['<rootDir>/src/tests/setupTests.ts'],
 	testEnvironment: 'jsdom',
 	moduleFileExtensions: ['js', 'ts', 'svelte'],
+	moduleNameMapper: {
+		'\\.(png|jpg|jpeg|gif|webp|avif|ico|bmp|svg)$': `<rootDir>/__mocks__/fileMock.js`,
+		'^.+\\.(css|sass|scss)$': '<rootDir>/__mocks__/styleMock.js'
+	},
 	testPathIgnorePatterns: ['/node_modules/', '/.svelte-kit'],
 	transformIgnorePatterns: ['/node_modules/'],
 	collectCoverageFrom: ['src/**/*.svelte']
