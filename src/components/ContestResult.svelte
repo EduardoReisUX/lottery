1e3a325409cbe82cb648cdd29f1a081d2ee06ffd
EduardoReisUX <ducabruno09@hotmail.com>
2022-04-04 16:27:51 -0400

    feat: add transitions to ContestResult

--- src/components/ContestResult.svelte
+++ src/components/ContestResult.svelte
@@ -1,16 +1,23 @@
 <script lang="ts">
 	export let dezenas: string[];
+
+	import { fly } from 'svelte/transition';
+	import { cubicInOut } from 'svelte/easing';
 </script>
 
 <div
 	class="relative flex flex-wrap justify-center items-center gap-6 
 				lg:row-start-2"
 >
-	{#each dezenas as dezena}
-		<strong
-			class="w-[4.75rem] h-[4.75rem] grid place-items-center bg-brand-white rounded-full font-bold text-lg text-brand-blackMuted"
-		>
-			{dezena}
-		</strong>
+	{#each dezenas as dezena, index}
+		{#key dezena + dezenas.length}
+			<strong
+				class="w-[4.75rem] h-[4.75rem] grid place-items-center bg-brand-white rounded-full font-bold text-lg text-brand-blackMuted"
+				in:fly={{ y: -40, duration: 150, delay: 150 * (index + 1), easing: cubicInOut }}
+				out:fly={{ duration: 0 }}
+			>
+				{dezena}
+			</strong>
+		{/key}
 	{/each}
 </div>
