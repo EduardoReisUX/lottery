54d7cc9066d7b681f8949f18193c297725f5593e
EduardoReisUX <ducabruno09@hotmail.com>
2022-04-04 20:02:32 -0400

    feat: add formatted date and better styling for ContestDate component

--- src/components/ContestDate.svelte
+++ src/components/ContestDate.svelte
@@ -3,10 +3,20 @@
 	export let data_concurso: Date;
 </script>
 
-<div class="text-center text-brand-white">
-	<p>
-		CONCURSO <span class="lg:hidden">Nº</span>
-		{numero_concurso}
-		<time class="hidden lg:inline-block">{data_concurso}</time>
+<div class="text-brand-white">
+	<p class="font-medium text-sm">
+		CONCURSO
+		<span class="lg:hidden">Nº</span>
+		<span class="lg:block lg:text-lg lg:font-bold">
+			{numero_concurso}
+			<time class="hidden lg:inline-block">
+				{' - '}
+				{new Date(data_concurso).toLocaleDateString('pt-br', {
+					day: '2-digit',
+					month: '2-digit',
+					year: 'numeric'
+				})}
+			</time>
+		</span>
 	</p>
 </div>
